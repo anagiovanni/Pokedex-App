ca9b0645797cf628a7ce6b2c755443a1ee8d63a8
anagiovanni <anagiovanni66@gmail.com>
2025-08-04 07:58:35 -0300

    Feito até a página 50

--- pokedex/components/Card/index.jsx
+++ pokedex/components/Card/index.jsx
@@ -1,6 +1,7 @@
 import { Image, View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { POKEMON_TYPE_COLORS } from "../../utils/colors";
 import { styles } from "./styles";
+import Button from "../Button";
 
 export default function Card({pokemon}) {
     return (
@@ -9,7 +10,16 @@ export default function Card({pokemon}) {
                 { backgroundColor: `${POKEMON_TYPE_COLORS[pokemon.Tipo[0].Nome]}`},
             ]}>
                 <View style={styles.info}>
-
+                    <Text style={styles.numero}>#{pokemon.numero}</Text>
+                    <Text style={styles.nome}>{pokemon.nome}</Text>
+                    <ScrollView horizontal>
+                        {pokemon.Tipo.map((tipo) => (
+                            <Button tipo={tipo} key={tipo.Nome} />
+                        ))}
+                    </ScrollView>
+                </View>
+                <View style={styles.imageContainer}>
+                    <Image source={{ uri: pokemon.Imagem }} style={styles.imagem }/>
                 </View>
             </TouchableOpacity>
     );
